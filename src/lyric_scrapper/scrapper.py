83299c0ed14259e26327032608e6012901f4173f
alexchung1233 <awchung@email.wm.edu>
2024-06-05 14:06:52 -0400

    Created logic to ingest txt file

--- src/lyric_scrapper/scrapper.py
+++ src/lyric_scrapper/scrapper.py
@@ -1,23 +1,41 @@
 import bs4
 import urllib.request as request
 import re
+import os
 
 BeautifulSoup = bs4.BeautifulSoup
 
-url = 'https://genius.com/Heize-and-july-lyrics'
-
-req = request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 
 
-html_page = request.urlopen(req).read()
+def get_genius_urls(filename: str) -> list[str]:
 
-soup = BeautifulSoup(html_page, 'html.parser')
+    song_urls_path =  f"{os.path.dirname(os.path.realpath(__file__))}/{filename}"
+    urls = []
+    with open(song_urls_path, "r") as file:
+        urls = file.readlines()
+    return urls
 
-verses = soup.find_all('div', attrs={'class', re.compile(r'Lyrics__Container*')})
-lyrics = []
-for verse in verses:
-    lyrics.extend(verse.stripped_strings)
 
-# lyrics = [verse for verse in verses.contents if verse != "<br/>"]
-for lyric in lyrics:
-    print(f'{lyric}')
+
+def scrape_genius_song_lyrics(url: str) -> list[str]:
+
+    req = request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    html_page = request.urlopen(req).read()
+    soup = BeautifulSoup(html_page, 'html.parser')
+    verses = soup.find_all('div', attrs={'class', re.compile(r'Lyrics__Container*')})
+    lyrics = []
+    for verse in verses:
+        lyrics.extend(verse.stripped_strings)
+    return lyrics
+
+
+def main():
+    urls = get_genius_urls(filename="genius-song-urls.txt")
+
+    for url in urls:
+        lyrics = scrape_genius_song_lyrics(url=url)
+        print(lyrics)
+
+
+if __name__  == "__main__":
+    main()
